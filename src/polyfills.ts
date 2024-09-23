15b4c4ff9326554c4e9340d02711cbc9736463e4
Balaraju Gandham <balarajugandham1@gmail.com>
2024-09-23 22:03:11 +0530

    Update polyfills.ts 2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,3 +4,18 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
+
+{
+  "extends": "./tsconfig.json",
+  "compilerOptions": {
+    "outDir": "./out-tsc/app",
+    "types": []
+  },
+  "include": [
+    "src/**/*.d.ts",
+    "src/**/*.ts",
+    "src/polyfills.ts",  // Ensure this line is present
+    "src/**/*.html",
+    "src/**/*.scss"
+  ]
+}
